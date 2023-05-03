bfab05ca2085888b0e4122c3b589bdda90e6422e
Triplix1 <vladikwww3vladik@gmail.com>
2023-05-03 11:34:05 +0300

    Update mappers

--- Campus/Campus.Core/MappingProfile/MapperProfile.cs
+++ Campus/Campus.Core/MappingProfile/MapperProfile.cs
@@ -8,6 +8,15 @@ public class MapperProfile : Profile
     public MapperProfile()
     {
         CreateMap<Student, Student>();
+        CreateMap<Group, Group>();
+        CreateMap<Faculty, Faculty>();
+        CreateMap<AcademicDisciplineGroup, AcademicDisciplineGroup>();
+        CreateMap<Academic, Academic>();
+        CreateMap<Cathedra, Cathedra>();
+        CreateMap<CurrentControl, CurrentControl>();
+        CreateMap<Discipline, Discipline>();
+        CreateMap<Speciality, Speciality>();
+        CreateMap<StudyProgram, StudyProgram>();
     }
     
 }
