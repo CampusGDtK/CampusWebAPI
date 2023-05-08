de4e77a508f358eb24c0d15c1994a21b9054dbc3
Triplix1 <vladikwww3vladik@gmail.com>
2023-05-08 23:13:15 +0300

    Added services contracts

---
+++ Campus/Campus.Core/ServiceContracts/IGroupService.cs
@@ -0,0 +1,19 @@
+﻿using Campus.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campus.Core.ServiceContracts
+{
+    public interface IGroupService
+    {
+        Task<GroupResponse> GetById(Guid groupId);
+        Task<IEnumerable<GroupResponse>> GetAll();
+        Task<IEnumerable<GroupResponse>> GetByFacultyId(Guid facultyId);
+        Task<GroupResponse> Add(GroupAddRequest groupAddRequest);
+        Task<GroupResponse> Update(GroupUpdateRequest groupUpdateRequest);
+        Task Delete(Guid groupId);
+    }
+}
---
+++ Campus/Campus.Core/ServiceContracts/IStudyProgramService.cs
@@ -0,0 +1,20 @@
+﻿using Campus.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campus.Core.ServiceContracts
+{
+    public interface IStudyProgramService
+    {
+        Task<StudyProgramResponse> GetById(Guid studyProgramId);
+        Task<IEnumerable<StudyProgramResponse>> GetAll();
+        Task<IEnumerable<StudyProgramResponse>> GetByFacultyId(Guid facultyId);
+        Task<IEnumerable<StudyProgramResponse>> GetBySpecialityId(Guid specialityId);
+        Task<StudyProgramResponse> Add(StudyProgramAddRequest studyProgramAddRequest);
+        Task<StudyProgramResponse> Update(StudyProgramUpdateRequest studyProgramUpdateRequest);
+        Task Delete(Guid studyProgramId);
+    }
+}
