7987e242d5a2f9d787be1bf041781a989eb89d18
VadimKostin <vadkostinxm@gmail.com>
2023-05-11 18:56:27 +0300

    Register services in DI container

--- Campus/Campus.Core/DependencyInjection.cs
+++ Campus/Campus.Core/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using Campus.Core.Domain.Entities;
 using Campus.Core.Domain.RepositoryContracts;
+using Campus.Core.ServiceContracts;
+using Campus.Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Campus.Core;
@@ -8,11 +10,22 @@ namespace Campus.Core;
 public static class DependencyInjection
 {
     public static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
-    
+
     public static IServiceCollection AddCore(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly);
-        
+        services.AddScoped<IAcademicService, AcademicService>();
+        services.AddScoped<IADGService, ADGService>();
+        services.AddScoped<ICathedraService, CathedraService>();
+        services.AddScoped<IDisciplineService, DisciplineService>();
+        services.AddScoped<IFacultyService, FacultyService>();
+        services.AddScoped<IGroupService, GroupService>();
+        services.AddScoped<IMarkingService, MarkingService>();
+        services.AddScoped<ISpecialityService, SpecialityService>();
+        services.AddScoped<IStudentService, StudentService>();
+        services.AddScoped<IStudyProgramService, StudyProgramService>();
+        services.AddScoped<ISyllabusService, SyllabusService>();
+
         return services;
     }
 }
