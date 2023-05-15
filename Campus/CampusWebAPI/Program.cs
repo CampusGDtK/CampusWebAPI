1a6320ae24c1ad3fd1d56a4c9e6ca7b8cb88df9a
VadimKostin <vadkostinxm@gmail.com>
2023-05-15 14:27:37 +0300

    Add ADGController

--- Campus/CampusWebAPI/Program.cs
+++ Campus/CampusWebAPI/Program.cs
@@ -1,7 +1,6 @@
 using Campus.Core;
 using Campus.Infrastructure;
 using Campus.Infrastructure.DataBaseContext;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,16 +12,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Campus WEB API", Version = "1.0" });
-});
-
-builder.Services.AddApiVersioning(config =>
-{
-    config.ApiVersionReader = new UrlSegmentApiVersionReader();
-});
-
-builder.Services.AddVersionedApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'V";
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
