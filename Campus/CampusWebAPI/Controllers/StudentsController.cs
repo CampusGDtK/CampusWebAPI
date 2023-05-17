50c9a56088df1869eb56446b0b5508fd305fc44e
dexon03 <dima0958757803@gmail.com>
2023-05-17 11:10:12 +0300

    added mark endpoints to student controller

--- Campus/CampusWebAPI/Controllers/StudentsController.cs
+++ Campus/CampusWebAPI/Controllers/StudentsController.cs
@@ -9,10 +9,12 @@ namespace CampusWebAPI.Controllers;
 public class StudentsController : ControllerBase
 {
     private readonly IStudentService _studentService;
+    private readonly IMarkingService _markingService;
 
-    public StudentsController(IStudentService studentService)
+    public StudentsController(IStudentService studentService, IMarkingService markingService)
     {
         _studentService = studentService;
+        _markingService = markingService;
     }
     
     [HttpGet]
@@ -49,4 +51,29 @@ public class StudentsController : ControllerBase
         await _studentService.Delete(id);
         return Ok();
     }
+    
+    [HttpGet("{studentId}/marks")]
+    public async Task<IActionResult> GetMarks(Guid studentId)
+    {
+        return Ok(await _markingService.GetByStudentId(studentId));
+    }
+    
+    [HttpGet("{studentId}/marks/{disciplineId}")]
+    public async Task<IActionResult> GetMarkInDiscipline(Guid studentId, Guid disciplineId)
+    {
+        return Ok(await _markingService.GetByStudentAndDisciplineId(studentId, disciplineId));
+    }
+    
+    [HttpPost("{studentId}/marks/{disciplineId}")]
+    public async Task<IActionResult> AddMark(Guid studentId, Guid disciplineId, [FromBody]IEnumerable<int> marks)
+    {
+        var markSetRequest = new MarkSetRequest
+        {
+            StudentId = studentId,
+            DisciplineId = disciplineId,
+            Marks = marks
+        };
+        return Ok(await _markingService.SetMark(markSetRequest));
+    }
+    
 }
