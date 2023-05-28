f1da5ae727d4a8b0504b291a834f53f36a5334f1
Daniil Dziubenko <freekick2017uk@gmail.com>
2023-05-28 16:49:23 +0300

    Validator was added

--- Campus/CampusWebAPI/ClientSide/react-app/src/App.js
+++ Campus/CampusWebAPI/ClientSide/react-app/src/App.js
@@ -3,13 +3,21 @@ import LogInPage from './components/LogInPage/LogInPage';
 import StudentView from './components/StudentView/StudentView';
 import AccountPage from './components/AccountPage/AccountPage';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [studentId, setStudentId] = useState();
+
+  function chooseStudentId(id) {
+    console.log(id);
+    setStudentId(id);
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LogInPage />}/>
-        <Route path='/student-view' element={<StudentView />}/>
+        <Route path='/' element={<LogInPage setStudentId={chooseStudentId} />}/>
+        <Route path='/student-view' element={<StudentView studentId={studentId}/>}/>
         <Route path='/profile' element={<AccountPage />}/>
       </Routes>
     </Router>
--- Campus/CampusWebAPI/ClientSide/react-app/src/components/LogInPage/LogInPage.js
+++ Campus/CampusWebAPI/ClientSide/react-app/src/components/LogInPage/LogInPage.js
@@ -1,13 +1,50 @@
 import {Link} from 'react-router-dom';
+import validator from 'validator';
 import './LogInPage.scss';
 
-function LogInPage() {
+function LogInPage({chooseStudentId}) {
+    function changeStudentId () {
+        const studentId = 'uuu111'
+        chooseStudentId(studentId);
+    }
+
+    function validationPassword () {
+        const password = document.querySelector('#password');
+        if (validator.isUUID(password.value)) {
+            console.log('password ok');
+        } else {
+            console.log('password error');
+        }
+        console.log(password.value);
+    }
+
+    function validationLogin () {
+        const login = document.querySelector('#login');
+        if (login.value.length < 4) {
+            console.log('login is too short');
+        } else if (login.value.length > 20) {
+            console.log('login is too long');
+        } else {
+            console.log('login is ok')
+        }
+    }
+
     return (
         <div className='mainDiv'>
             <div className='wrapperDiv'>
                 <p>Campus 3.0</p>
-                <input type='text' required placeholder='Type your login'></input>
-                <input type='password' required placeholder='Type your password'></input>
+                <input 
+                    onInput={validationLogin}
+                    type='text' 
+                    required 
+                    placeholder='Type your login'
+                    id='login'></input>
+                <input 
+                    onInput={validationPassword} 
+                    type='password' 
+                    required 
+                    placeholder='Type your password' 
+                    id='password'></input>
                 <Link to='/student-view'>
                     <button>LOGIN</button>
                 </Link>
